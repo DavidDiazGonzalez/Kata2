e0b3f79137b4323d441910e9b0019bf4f1fc2609
usuario <usuario@10.22.146.196>
2013-10-04 12:35:37 +0100

    Kata2.2

--- src/kata2/main.java
+++ src/kata2/main.java
@@ -2,23 +2,27 @@ package kata2;
 
 import java.util.ArrayList;
 
-
 public class main {
 
-   
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList <>();
-        
-        
+        ArrayList<Integer> list = new ArrayList<>();
+
+
         for (int i = 0; i < 100; i++) {
-            list.add (i);
-            
-            for (Integer value : list) {
-                System.out.println(value);
+            list.add(i);
+        }
+        System.out.println("De la lista: ");
+        for (Integer value : list) {
+            System.out.println(value);
+        }
+        ArrayList<Integer> listaPares = new ArrayList<>();
+        for (Integer value : list) {
+            if (value % 2 == 0) {
+                listaPares.add(value);
             }
-            
-            
-            
+        }
+        for (Integer value : listaPares) {
+            System.out.println("Los numeros pares son: " + value);
         }
     }
 }
