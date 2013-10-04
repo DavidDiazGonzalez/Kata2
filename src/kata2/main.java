8ad44fe041c07c1e3224c7dcf9804a978f23015d
usuario <usuario@10.22.146.196>
2013-10-04 12:46:04 +0100

    Kata2.3

--- src/kata2/main.java
+++ src/kata2/main.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class main {
 
     public static void main(String[] args) {
+        
+        int[] vector = new int[54];
         ArrayList<Integer> list = new ArrayList<>();
 
 
@@ -23,6 +25,21 @@ public class main {
         }
         for (Integer value : listaPares) {
             System.out.println("Los numeros pares son: " + value);
+        }
+        for (int i = 0; i < vector.length; i++) {
+             vector[i] = (int)(Math.random()*100);
+             
+        }
+        System.out.println("Tras una encuesta para saber la edad de la población se han recogido las siguientes edades: ");
+        
+        for (Integer value : vector) {
+            System.out.println(value);
+            
+        }
+        System.out.println("Los que están jubilados tienen: ");
+        for (Integer value : vector) {
+            if (value > 57) 
+                System.out.println(value + " años");
         }
     }
 }
